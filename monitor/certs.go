964c45f4ce87eab36184b18f2581d8e1095b6812
k1nky <avshalashov@yandex.ru>
2020-03-07 18:18:05 +0300

    Change fingerprint format (without spaces)

--- monitor/certs.go
+++ monitor/certs.go
@@ -11,6 +11,7 @@ import (
 	"time"
 )
 
+// CheckCertificate checks certificate chain for the host
 func CheckCertificate(cert *x509.Certificate, hostname string) error {
 	if time.Now().After(cert.NotAfter) || time.Now().Before(cert.NotBefore) {
 		msg := fmt.Sprintf("Certificate %s is expired or inactived yet", cert.Subject.CommonName)
@@ -23,31 +24,33 @@ func CheckCertificate(cert *x509.Certificate, hostname string) error {
 }
 
 func fingerprint(data []byte) string {
-	return fmt.Sprintf("% x", sha1.Sum(data))
+	return fmt.Sprintf("%x", sha1.Sum(data))
 }
 
-// GetCertificates is return the full certificate chain from tls connection
+// GetCertificates returns the full certificate chain from TLS connection
 func (mon Monitor) GetCertificates(host string, sni string) []*x509.Certificate {
 
 	timeout := time.Duration(mon.Ctx.TLSTimeout) * time.Second
 	tcpConn, err := net.DialTimeout("tcp", host, timeout)
 	if err != nil {
-		log.Println("Failed to establish TCP connection: ", err)
+		log.Printf("Failed to establish TCP connection to %s: %s\n", host, err)
 		return nil
 	}
+	defer tcpConn.Close()
+
 	tlsConn := tls.Client(tcpConn, &tls.Config{
 		InsecureSkipVerify: true,
 		ServerName:         sni,
 	})
-	defer tcpConn.Close()
 	if tlsConn == nil {
-		log.Println("Can not create TLS client: ", err)
+		log.Printf("Can not create TLS client for %s: %s\n", host, err)
 		return nil
 	}
 	defer tlsConn.Close()
+
 	tlsConn.SetDeadline(time.Now().Add(timeout))
 	if err := tlsConn.Handshake(); err != nil {
-		log.Println("Failed to handshake: ", err)
+		log.Printf("Failed to handshake with %s: %s\n", host, err)
 		return nil
 	}
 	state := tlsConn.ConnectionState()
